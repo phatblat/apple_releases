5451f495adcde0b56c0657d1091ac57db3d3ae90
Ben Chatelain <ben@octop.ad>
2022-08-13 10:27:52 -0600

    ✨ Add example http request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    get("https://httpbin.org/ip".to_string())
+        .unwrap();
+        // .json::<HashMap<String, String>>()
+        // .unwrap()
+        // .get("origin")
+        // .unwrap()
+        // .to_string();
+}
+
+fn get(url: String) -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::blocking::get(url)?
+        .json::<HashMap<String, String>>()?;
+    println!("{:#?}", resp);
+    Ok(())
 }
