291827dd57af03fda7aabf4ade2c4fb891f788cf
Ben Chatelain <ben@octop.ad>
2022-08-14 13:59:28 -0600

    ✅ Add tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-//
-// main.rs
-//
+//!
+//! main.rs
+//!
 
 use scraper::{Html, Selector};
 use scraper::html::Select;
@@ -64,4 +64,47 @@ fn find_articles(content: String) {
             None => continue,
         }
     }
+}
+
+#[test]
+fn test_get() {
+    let apple_dev_news_updates = "https://developer.apple.com/news/releases/";
+    let body = get(apple_dev_news_updates.to_string()).unwrap();
+    assert!(body.len() > 0);
+}
+
+#[test]
+fn test_parse() {
+    let html = r###"
+<section class="article-content-container column large-9 medium-9 small-12 no-padding-left padding-right-small padding-top padding-bottom-small divider-top">
+    <a class="article-title external-link" href="/download/"><h2>Xcode 14 beta 5 (14A5294e)</h2></a>
+    <div class="article-text-wrapper">
+        <p class="lighter  article-date">August 8, 2022</p>
+        <span class="article-text">
+            <p> <a href="/download/applications" class="more">View downloads</a></p>
+            <il>
+                <p><a href="/go/?id=xcode-14-sdk-rn" class="more">View release notes</a></p>
+            </il>
+        </span>
+    </div>
+    <section class="social-share-container">
+        <ul class="sharesheet-options">
+            <li class="social-option">
+                <button class="icon icon-facebook social-icon" data-href="https://developer.apple.com/news/releases/?id=08082022a" data-share-type="facebook" aria-label="Share via Facebook"> </button>
+            </li>
+            <li class="social-option">
+                <button class="icon icon-twitter social-icon" data-href="https://developer.apple.com/news/releases/?id=08082022a" data-share-type="twitter" aria-label="Share via Twitter"></button>
+            </li>
+            <li class="social-option">
+                <button class="icon icon-mail social-icon" data-href="https://developer.apple.com/news/releases/?id=08082022a" data-share-type="mail" data-title="Xcode 14 beta 5 (14A5294e) - Releases - Apple Developer" data-description="https://developer.apple.com/news/releases/?id=08082022a" aria-label="Share via mail"></button>
+            </li>
+            <li class="social-option">
+                <button class="icon icon-link social-icon" data-href="https://developer.apple.com/news/releases/?id=08082022a" data-share-type="copy" data-copy-title="Copied to clipboard" aria-label="Share via link"></button>
+            </li>
+        </ul>
+    </section>
+</section>
+    "###.to_string();
+
+    find_articles(html);
 }
