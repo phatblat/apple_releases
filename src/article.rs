984a6f8562c8c7072ae60735727bc56dfd083374
Ben Chatelain <ben@octop.ad>
2022-11-28 20:56:22 -0700

    💡 Document Article

--- src/article.rs
+++ src/article.rs
@@ -7,15 +7,25 @@ use url::Url;
 
 /// An article from the Apple Developer software releases site.
 pub struct Article {
+    /// The article title. For an OS release, this is the OS version.
+    /// Example: iOS 16.2 beta 3 (20C5049e)
     pub title: String,
+
+    /// The article date. Example: November 15, 2022
     pub date: String,
+
+    /// The release notes URL. This will be `None` if the article does not have release notes.
+    /// Release note URLs end in a path like `/go/?id=ios-16.2-rn`
     pub release_notes_url: Option<Url>,
 }
 
 impl Article {
-    /// Creates a new article.
+    /// "Unfurls" a releases notes URL to the final page URL. These aren't HTTP redirects,
+    /// but rather in-page JavaScript redirects. The final URL path looks something like the
+    /// following: `/documentation/ios-ipados-release-notes/ios-ipados-16_2-release-notes`.
     pub fn release_notes_url_unfurled(&self) -> Option<Url> {
-        self.release_notes_url.as_ref()
+        self.release_notes_url
+            .as_ref()
             .map(|url| crate::url::unfurl(url).unwrap())
     }
 }
